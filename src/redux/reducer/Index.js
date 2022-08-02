9f7f36dc07b03f116af6b5f06a168a2a4fafaa35
NITHISHGM <nithishkumars@geakminds.com>
2022-08-02 10:48:33 +0530

    prod deploy

--- src/redux/reducer/Index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-
-//async
-import { FETCH_PRODUCTS, FETCH_PRODUCT } from "./SetListProduct";
-const reducer = combineReducers({
-  FETCH_PRODUCTS: FETCH_PRODUCTS,
-  FETCH_PRODUCT: FETCH_PRODUCT,
-});
-export default reducer;
